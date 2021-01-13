fb39d226b6b95231330ae2a05587fed3c09c2661
stgatilov <none@none>
2021-01-13 15:42:36 +0700

    Some renames.

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
+import MainGui from "./MainGui";
 
 document.addEventListener("DOMContentLoaded", function() {
   ReactDOM.render(
-    React.createElement(Counter),
+    React.createElement(MainGui),
     document.getElementById("mount")
   );
 });
